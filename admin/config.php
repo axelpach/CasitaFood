01114842936cefa93fc7f15585651efc64e77c3c
axelpach <axel.pacheco.tellez@gmail.com>
2019-05-26 10:33:29 -0500

    Create config.php

---
+++ admin/config.php
@@ -0,0 +1,13 @@
+<?php 
+
+define ('RUTA', 'http://localhost/CasitaFood/');
+
+$bd_config = array(
+	'basedatos' => 'casita-food',
+	'usuario' => 'root', 
+	'pass' => ''
+);
+
+$css_general_version = 'v1.0';
+
+ ?>
